29d1c92e69afd737e78e084b6adc5e94c2713ddd
pgm-thabisadingani <71275598+pgm-thabisadingani@users.noreply.github.com>
2022-06-03 16:26:47 +0200

    working on the challenge camera screen

--- app/screens/ChallengeDetailsScreen.js
+++ app/screens/ChallengeDetailsScreen.js
@@ -1,15 +1,14 @@
 import { doc, getDoc } from "firebase/firestore";
 import React, { useState, useEffect } from "react";
 import { StyleSheet, Text } from "react-native";
-import { Error, Icon, LoadingIndicator, View } from "../components";
+import { Error, LoadingIndicator, View } from "../components";
 import { ChallengeDetails } from "../components/challenges";
-import { Colors, db } from "../config";
+import { db } from "../config";
 
 export const ChallengeDetailsScreen = ({ route, navigation }) => {
   const [challenge, setChallenge] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
-  const [show, setShow] = useState(false);
   const challengeUid = route.params;
 
   /*getting challenges by doc id */
@@ -23,11 +22,9 @@ export const ChallengeDetailsScreen = ({ route, navigation }) => {
       // doc.data() will be undefined in this case
       console.log("No such document!");
       setIsError(true);
-      setIsLoading(false);
+      setIsLoading(true);
     }
   };
-
-  const handleShowAdd = () => {};
 
   useEffect(() => {
     const unsubscribe = getChallengeAsync();
@@ -47,7 +44,6 @@ export const ChallengeDetailsScreen = ({ route, navigation }) => {
       setIsError(error.message);
     }
   };
-  // <ChallengeDetails city={removeSpace(challenge.eventLocation)} />;
 
   return isError ? (
     <Error>{isError}</Error>
@@ -57,40 +53,8 @@ export const ChallengeDetailsScreen = ({ route, navigation }) => {
     <View isSafe style={styles.container}>
       <Text>Challenge details</Text>
       <Text>{challengeUid}</Text>
-      <View style={{ alignItems: "flex-end" }}>
-        <Icon
-          name="window-close"
-          size={30}
-          color={Colors.mediumGray}
-          onPress={() => setShow(true)}
-        />
-      </View>
-      {show ? (
-        <View
-          style={{
-            backgroundColor: " rgba(0, 0, 0, 0.7)",
-            height: "100%",
-            width: "100%",
-            justifyContent: "center",
-            alignItems: "center",
-            opacity: 0.4,
-          }}
-        >
-          <View style={{ alignItems: "flex-end" }}>
-            <Icon
-              name="window-close"
-              size={30}
-              color={Colors.mediumGray}
-              onPress={() => setShow(false)}
-            />
-          </View>
-          <View>
-            <Text style={{ fontSize: 30, color: Colors.white, opacity: 1 }}>
-              Show me
-            </Text>
-          </View>
-        </View>
-      ) : null}
+
+      <ChallengeDetails city={removeSpace(challenge.eventLocation)} />
     </View>
   );
 };
